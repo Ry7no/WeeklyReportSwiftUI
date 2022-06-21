d68affb8ec5114a8b8b45c927a7279f70cce6cfc
RyanChen <ry4novak@gmail.com>
2022-06-21 11:10:06 +0800

    Initial Commit

---
+++ WeeklyReportSwiftUI/WeeklyReportSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeklyReportSwiftUIApp.swift
+//  WeeklyReportSwiftUI
+//
+//  Created by Ryan@work on 2022/6/21.
+//
+
+import SwiftUI
+
+@main
+struct WeeklyReportSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
